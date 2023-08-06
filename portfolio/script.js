3707d0e29e1cfd00450cca9995714fb2a8e0474d
krishu00 <130180810+krishu00@users.noreply.github.com>
2023-08-07 00:38:29 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,31 @@
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
+
+// "header-text / dif-state-------
+var state = new state(".dif-state", {
+    String: ["Coder", "Developer", " ", "FrontEnd Developer", "Designer", ""],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
